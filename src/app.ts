da0ab86621fa1b1a91f1f0a7f0827932a4c5c692
Artrudjob <Artrudjob@yandex.ru>
2022-11-30 15:37:22 +0300

    feat: create app

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
