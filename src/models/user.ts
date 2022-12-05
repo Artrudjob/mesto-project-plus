d3643cb9a049eb368aac0e08cf4a8945674d8c34
Artrudjob <Artrudjob@yandex.ru>
2022-12-05 14:17:20 +0300

    feat: add validate url for avatar

--- src/models/user.ts
+++ src/models/user.ts
@@ -10,7 +10,12 @@ const userSchema = new Schema(
   {
     name: {type: String, minlength: 2, maxlength: 30, required: true},
     about: {type: String, minlength: 2, maxlength: 200, required: true},
-    avatar: {type: String, required: true}
+    avatar: {type: String, required: true, validate: {
+      validator(value: string) {
+        return value.match(`^(https?|http):/`) !== null;
+      },
+        message: 'Необходимо ввести url'
+      }}
   }
 )
 
